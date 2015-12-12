8ba5ddd11619493d35ada55b27ad1a99e053f823
BuildTools <mreilaender@student.tgm.ac.at>
2015-12-12 17:32:13 +0100

    first commit

--- src/main/java/org/example/www/ExampleMain.java
+++ src/main/java/org/example/www/ExampleMain.java
@@ -1,18 +1,65 @@
 package org.example.www;
 
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.example.www.command.CommandManager;
+import org.example.www.command.handler.ExampleCommand;
 
 /**
  * @author mreilaender
  */
 public class ExampleMain extends JavaPlugin {
+    private String name;
+    private String prefix;
+    private PluginDescriptionFile pluginDescriptionFile;
+    private CommandManager commandManager;
+
+
     @Override
     public void onEnable() {
         getServer().getConsoleSender().sendMessage("Hello World");
+
+        this.pluginDescriptionFile = this.getDescription();
+        this.name = this.pluginDescriptionFile.getName();
+        this.prefix = this.pluginDescriptionFile.getPrefix();
+        this.commandManager = new CommandManager(this);
+        this.registerCommandExecuter();
+
+        this.commandManager.addCommandHandler("test", new ExampleCommand());
+    }
+
+    private void registerCommandExecuter() {
+        getCommand(this.prefix).setExecutor(this.commandManager);
+        //getCommand(Character.toString(this.prefix.charAt(0))).setExecutor(this.commandManager);
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public String getPluginName() {
+        return this.name;
+    }
+
+    public String getPluginDescription() {
+        if(!(this.pluginDescriptionFile.getDescription() == null))
+            return this.pluginDescriptionFile.getDescription();
+        else
+            return ErrorMessages.noPluginDescription;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    /**
+     * Formats with colors and returns the plugins prefix, so it can be used for printing to console.
+     * Example: [prefix]
+     * @return String
+     */
+    public String getChatPrefix() {
+        return ChatColor.BLUE + "[" + prefix + "] " + ChatColor.RESET;
     }
 }
---
+++ src/main/java/org/example/www/command/CommandManager.java
@@ -0,0 +1,59 @@
+package org.example.www.command;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.example.www.ErrorMessages;
+import org.example.www.ExampleMain;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author mreilaender
+ * @version 12.12.2015
+ */
+public class CommandManager implements CommandExecutor {
+    private Map<String, Command> commandHandlers;
+    private ExampleMain main;
+
+    public CommandManager(ExampleMain main) {
+        this.commandHandlers = new HashMap<String, Command>();
+        this.main = main;
+    }
+
+    public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command, String cmdLabel, String[] args) {
+        if(cmdLabel.equalsIgnoreCase(this.main.getPrefix()) || cmdLabel.equalsIgnoreCase(Character.toString(this.main.getPrefix().charAt(0)))) {
+            if(commandSender instanceof Player) {
+                Player player = (Player) commandSender;
+
+                if(args.length == 0) {
+                    player.sendMessage(this.main.getPluginDescription());
+                } else {
+                    String subCmd = args[0];
+                    Command subCmdAbs = this.commandHandlers.get(subCmd);
+                    if(subCmdAbs != null) {
+                        String[] subCmdArgs = Arrays.copyOfRange(args, 1, args.length);
+                        boolean succeed = subCmdAbs.onCommand(subCmd, subCmdArgs, player);
+                        if(!succeed)
+                            player.sendMessage(this.main.getChatPrefix() + ErrorMessages.errPerfCommand);
+                    } else
+                        player.sendMessage(this.main.getChatPrefix() + ErrorMessages.noSuchCommand + subCmd);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public void addCommandHandler(String label, Command command) {
+        if(!this.commandHandlers.containsKey(label)) {
+            if(!this.commandHandlers.containsValue(command)) {
+                this.commandHandlers.put(label, command);
+            }
+        }
+    }
+
+
+}
