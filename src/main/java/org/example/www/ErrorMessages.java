10b2d35659c50aa1b016cd53e2e29b772b5c81a8
BuildTools <mreilaender@student.tgm.ac.at>
2015-12-12 17:30:32 +0100

    error messages for logging or sth.

---
+++ src/main/java/org/example/www/ErrorMessages.java
@@ -0,0 +1,10 @@
+package org.example.www;
+
+/**
+ * @author mreilaender
+ */
+public interface ErrorMessages {
+    String errPerfCommand = "Error when performing this command. Please contact an Administrator";
+    String noSuchCommand = "There is not command like ";
+    String noPluginDescription = "No Plugin description defined in the PluginDescriptionFile";
+}
