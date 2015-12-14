365dba1d9a6d7b2b65556cfc45a77df4fd78e489
BuildTools <mreilaender@student.tgm.ac.at>
2015-12-14 15:00:51 +0100

    structure

--- src/main/java/org/doesntexist/limitx/Generic.java
+++ src/main/java/org/doesntexist/limitx/Generic.java
@@ -2,9 +2,12 @@ package org.doesntexist.limitx;
 
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.doesntexist.limitx.command.handler.ExampleCommand;
 import org.doesntexist.limitx.command.CommandManager;
+import org.doesntexist.limitx.event.PlayerDeathListener;
+import org.doesntexist.limitx.event.PlayerJoinListener;
 import org.doesntexist.limitx.utils.GenericFactory;
 
 /**
@@ -15,7 +18,7 @@ public class Generic extends JavaPlugin {
     private String prefix;
     private PluginDescriptionFile pluginDescriptionFile;
     private CommandManager commandManager;
-
+    private PluginManager pluginManager;
 
     @Override
     public void onEnable() {
@@ -26,11 +29,31 @@ public class Generic extends JavaPlugin {
         this.name = this.pluginDescriptionFile.getName();
         this.prefix = this.pluginDescriptionFile.getPrefix();
         this.commandManager = new CommandManager();
-        this.registerCommandExecuter();
+        this.pluginManager = this.getServer().getPluginManager();
 
-        this.commandManager.addCommandHandler("test", new ExampleCommand());
+        this.registerCommandExecuter();
+        this.registerListener();
+        this.registerCommands();
+
     }
 
+    /**
+     * Registers all Listener in PluginManager
+     */
+    private void registerListener() {
+        this.pluginManager.registerEvents(new PlayerJoinListener(), this);
+        this.pluginManager.registerEvents(new PlayerDeathListener(), this);
+    }
+
+    /**
+     *
+     */
+    private void registerCommands() {
+        this.commandManager.addCommandHandler("test", new ExampleCommand());
+    }
+    /**
+     * Registers command executer
+     */
     private void registerCommandExecuter() {
         getCommand(this.prefix).setExecutor(this.commandManager);
         //getCommand(Character.toString(this.prefix.charAt(0))).setExecutor(this.commandManager);
