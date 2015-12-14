d8809f07f2e6c9984485e1ac61aca996388f20eb
BuildTools <mreilaender@student.tgm.ac.at>
2015-12-14 12:20:03 +0100

    bugfix

--- src/main/java/org/doesntexist/limitx/Messages.java
+++ src/main/java/org/doesntexist/limitx/Messages.java
@@ -7,6 +7,6 @@ public interface Messages {
     String errPerfCommand = "Error when performing this command. Please contact an Administrator";
     String noSuchCommand = "There is not command like ";
     String noPluginDescription = "No Plugin description defined in the PluginDescriptionFile";
-    String welcomeBackMessage = "Welcome back %s\n" + "You can show a list of useful commands cia /general commands";
-    String welcomeNewMessage = "Hello %s\n" + "\"Please register to our Server in order to build. You can register via /general register";
+    String welcomeBackMessage = "Welcome back %s\n" + "You can show a list of useful commands cia /generic commands";
+    String welcomeNewMessage = "Hello %s\n" + "\"Please register to our Server in order to build. You can register via /generic register";
 }
